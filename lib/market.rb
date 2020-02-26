58e67d1343840db30ef0932e3a4403404046aa09
Ryan Camp <cmpprg@icloud.com>
2020-02-26 11:59:48 -0700

    Create #total_inventory for market class

--- lib/market.rb
+++ lib/market.rb
@@ -18,4 +18,25 @@ class Market
   def vendors_that_sell(item)
     @vendors.find_all{|vendor| vendor.inventory.include?(item)}
   end
+
+  def total_inventory
+    all_inventories = @vendors.map do |vendor|
+       vendor.inventory
+     end
+
+    items = @vendors.map{ |vendor| vendor.inventory.keys }.flatten.uniq
+
+    total_inventory = items.reduce({}) do |total_inventory, item|
+      total_inventory[item] = {quantity: 0, vendors: vendors_that_sell(item)}
+      total_inventory
+    end
+    
+    all_inventories.each do |inventory|
+      inventory.each do |item, quantity|
+        total_inventory[item][:quantity] += quantity
+      end
+    end
+
+    total_inventory
+  end
 end
--- test/market_test.rb
+++ test/market_test.rb
@@ -15,11 +15,7 @@ class ItemTest < Minitest::Test
     @vendor2 = Vendor.new("Ba-Nom-a-Nom")
     @vendor3 = Vendor.new("Palisade Peach Shack")
     @market = Market.new("South Pearl Street Farmers Market")
-    @vendor1.stock(@item1, 35)
-    @vendor1.stock(@item2, 7)
-    @vendor2.stock(@item4, 50)
-    @vendor2.stock(@item3, 50)
-    @vendor3.stock(@item1, 65)
+
   end
 
   def test_it_exists
@@ -49,12 +45,38 @@ class ItemTest < Minitest::Test
   end
 
   def test_it_can_compile_a_list_of_vendors_that_sell_an_item
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 50)
+    @vendor3.stock(@item1, 65)
     @market.add_vendor(@vendor1)
     @market.add_vendor(@vendor2)
     @market.add_vendor(@vendor3)
 
     assert_equal [@vendor1, @vendor3], @market.vendors_that_sell(@item1)
     assert_equal [@vendor2], @market.vendors_that_sell(@item4)
+  end
+
+  def test_it_can_display_total_inventory
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
+    @vendor3.stock(@item3, 10)
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
+    expected = {
+                  @item1=> {quantity:100, vendors: @market.vendors_that_sell(@item1)},
+                  @item2=> {quantity:7, vendors: @market.vendors_that_sell(@item2)},
+                  @item4=> {quantity:50, vendors: @market.vendors_that_sell(@item4)},
+                  @item3=> {quantity:35, vendors: @market.vendors_that_sell(@item3)},
+
+    }
+    assert_equal expected, @market.total_inventory
   end
 
 end
